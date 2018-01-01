ac618835e39d007320c6ca85f9cbd133a981bf01
zwolf21 <pbr112@naver.com>
2018-01-02 03:03:21 +0900

    setup.py version changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='dform',
-    version='0.0.2',
+    version='0.0.3',
     description='SQL ORM API for pandas DataFrame #tag: list dict pandas dataframe dict-list list of dict record',
     author = 'HS Moon',
     author_email = 'mhs9089@gmail.com',
